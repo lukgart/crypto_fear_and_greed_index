9624c09fa9a692f6009081a048dd4ac5a8935300
Lukas <87695904+lukgart@users.noreply.github.com>
2024-06-08 18:19:14 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+import requests
+
+def fetch_fear_and_greed_index():
+    url = "https://api.alternative.me/fng/"
+    response = requests.get(url)
+    data = response.json()
+
+    value = data['data'][0]['value']
+    value_classification = data['data'][0]['value_classification']
+
+    print(f"Fear and Greed Index Value: {value}")
+    print(f"Classification: {value_classification}")
+
+if __name__ == "__main__":
+    fetch_fear_and_greed_index()
