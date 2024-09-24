aa36cd095a0b52dbe3172d82941763796a0c3c92
Lukas Work <lukas.gartner@gmail.com>
2024-09-24 15:50:02 +0200

    Git Test

--- main.py
+++ main.py
@@ -7,10 +7,8 @@ def fetch_fear_and_greed_index():
 
     value = data['data'][0]['value']
     value_classification = data['data'][0]['value_classification']
-    print('______________________________________')
     print(f"Fear and Greed Index Value: {value}")
     print(f"Classification: {value_classification}")
-    print('______________________________________')
 
 if __name__ == "__main__":
     fetch_fear_and_greed_index()
